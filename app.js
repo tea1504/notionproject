7d4e5bafa3577608429ddc2914f49189029df98d
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2024-08-03 18:29:00 +0700

    [update] them moi truong

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require("dotenv").config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const { Client } = require("@notionhq/client")
+const notion = new Client({ auth: process.env.NOTION_KEY })
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
